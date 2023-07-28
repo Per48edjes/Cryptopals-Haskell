14b50f6f1d662cc1f0a15a22ec68996f7da02dbf
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-27 22:44:04 -0700

    Finish Challenge 2, Challenge Set 1

--- app/ChallengeSet1.hs
+++ app/ChallengeSet1.hs
@@ -3,16 +3,18 @@
 module ChallengeSet1 (main) where
 
 import Data.ByteString (ByteString)
-import Data.ByteString.Base16 qualified as B16
-import Data.ByteString.Base64 qualified as B64
+import Lib
 
 main :: IO ()
-main = print (hexToBase64 c1HexString)
+main = do
+    print $ hexToBase64 c1HexString
+    print $ fixedXor c2HexString1 c2HexString2
 
 c1HexString :: ByteString
 c1HexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-hexToBase64 :: ByteString -> ByteString
-hexToBase64 hexByteString = case B16.decode hexByteString of
-    Right decoded -> B64.encode decoded
-    _ -> error "Invalid hex string"
+c2HexString1 :: ByteString
+c2HexString1 = "1c0111001f010100061a024b53535009181c"
+
+c2HexString2 :: ByteString
+c2HexString2 = "686974207468652062756c6c277320657965"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,16 @@
-module Lib () where
+module Lib (module Lib) where
+
+import Data.Bits (xor)
+import Data.ByteString (ByteString, pack, unpack)
+import Data.ByteString.Base16 qualified as B16
+import Data.ByteString.Base64 qualified as B64
+
+hexToBase64 :: ByteString -> Either String ByteString
+hexToBase64 hexByteString = B64.encode <$> B16.decode hexByteString
+
+fixedXor :: ByteString -> ByteString -> Either String ByteString
+fixedXor bs1 bs2 =
+    B16.encode <$> do
+        decoded1 <- B16.decode bs1
+        decoded2 <- B16.decode bs2
+        return $ pack $ zipWith xor (unpack decoded1) (unpack decoded2)
