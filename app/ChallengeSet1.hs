7dfadc6332bb35d9a3aa1b4bbd973b7e06848c9e
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-30 03:45:40 -0700

    Start Challenge 3, Challenge Set 1

--- app/ChallengeSet1.hs
+++ app/ChallengeSet1.hs
@@ -2,19 +2,29 @@
 
 module ChallengeSet1 (main) where
 
-import Data.ByteString (ByteString)
+import Data.ByteString qualified as B (ByteString)
+import Data.ByteString.Base16 qualified as B16
+import Data.Serialize qualified as S
+import Data.Word (Word8)
 import Lib
 
 main :: IO ()
 main = do
     print $ hexToBase64 c1HexString
-    print $ fixedXor c2HexString1 c2HexString2
+    print $ applyXor c2HexString1 c2HexString2
+    print $ map (applyXor c3HexString) c3CandidateKeys
 
-c1HexString :: ByteString
+c1HexString :: B.ByteString
 c1HexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-c2HexString1 :: ByteString
+c2HexString1 :: B.ByteString
 c2HexString1 = "1c0111001f010100061a024b53535009181c"
 
-c2HexString2 :: ByteString
+c2HexString2 :: B.ByteString
 c2HexString2 = "686974207468652062756c6c277320657965"
+
+c3HexString :: B.ByteString
+c3HexString = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+c3CandidateKeys :: [B.ByteString]
+c3CandidateKeys = map (B16.encode . S.encode) ([0 .. 255] :: [Word8])
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,19 @@
 module Lib (module Lib) where
 
 import Data.Bits (xor)
-import Data.ByteString (ByteString, pack, unpack)
+import Data.ByteString qualified as B
 import Data.ByteString.Base16 qualified as B16
 import Data.ByteString.Base64 qualified as B64
 
-hexToBase64 :: ByteString -> Either String ByteString
+hexToBase64 :: B.ByteString -> Either String B.ByteString
 hexToBase64 hexByteString = B64.encode <$> B16.decode hexByteString
 
-fixedXor :: ByteString -> ByteString -> Either String ByteString
-fixedXor bs1 bs2 =
+applyXor :: B.ByteString -> B.ByteString -> Either String B.ByteString
+applyXor hexByteString hexKey =
     B16.encode <$> do
-        decoded1 <- B16.decode bs1
-        decoded2 <- B16.decode bs2
-        return $ pack $ zipWith xor (unpack decoded1) (unpack decoded2)
+        decodedBs <- B16.decode hexByteString
+        decodedKey <- B16.decode hexKey
+        return $ B.pack $ zipWith xor (B.unpack decodedBs) (cycle $ B.unpack decodedKey)
+
+-- Function that scores plaintext
+--
