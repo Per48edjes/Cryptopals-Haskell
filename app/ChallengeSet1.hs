5cfeb2d4e0156bacf0caeffe67e08d14d04756b9
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-30 03:45:40 -0700

    Finish Challenge 3, Challenge Set 1

--- app/ChallengeSet1.hs
+++ app/ChallengeSet1.hs
@@ -2,19 +2,40 @@
 
 module ChallengeSet1 (main) where
 
-import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+import Data.Function (on)
+import Data.List qualified as L
+import Data.Maybe
+import Data.Word (Word8)
 import Lib
+import Text.Hex
 
 main :: IO ()
 main = do
-    print $ hexToBase64 c1HexString
-    print $ fixedXor c2HexString1 c2HexString2
+    print challenge1
+    print challenge2
+    challenge3 <- challenge3IO
+    print challenge3
 
-c1HexString :: ByteString
-c1HexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+challenge1 :: B.ByteString
+challenge1 = fromJust $ hexToBase64 c1HexString
+  where
+    c1HexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-c2HexString1 :: ByteString
-c2HexString1 = "1c0111001f010100061a024b53535009181c"
+challenge2 :: B.ByteString
+challenge2 = fromJust $ applyXor c2HexString1 c2HexString2
+  where
+    c2HexString1 = "1c0111001f010100061a024b53535009181c"
+    c2HexString2 = "686974207468652062756c6c277320657965"
 
-c2HexString2 :: ByteString
-c2HexString2 = "686974207468652062756c6c277320657965"
+challenge3IO :: IO B.ByteString
+challenge3IO = do
+    charFreqs <- getCharFreqs "data/char-freqs.csv"
+    let englishScore = charFreqScore charFreqs
+     in return $ L.maximumBy (compare `on` englishScore) c3CandidatePlaintexts
+  where
+    c3HexString = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    c3CandidateKeys :: [Text]
+    c3CandidateKeys = map (encodeHex . B.singleton) ([0 .. 255] :: [Word8])
+    c3CandidatePlaintexts :: [B.ByteString]
+    c3CandidatePlaintexts = mapMaybe (applyXor c3HexString) c3CandidateKeys
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,50 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib (module Lib) where
 
 import Data.Bits (xor)
-import Data.ByteString (ByteString, pack, unpack)
-import Data.ByteString.Base16 qualified as B16
+import Data.ByteString qualified as B
 import Data.ByteString.Base64 qualified as B64
+import Data.ByteString.Lazy qualified as BL
+import Data.Csv qualified as CSV
+import Data.Csv.Incremental
+import Data.Map qualified as M
+import Data.Text.Encoding
+import Data.Vector qualified as V
+import Data.Word
+import GHC.Generics
+import Text.Hex
 
-hexToBase64 :: ByteString -> Either String ByteString
-hexToBase64 hexByteString = B64.encode <$> B16.decode hexByteString
+-- Convert a hex string to base64
+hexToBase64 :: Text -> Maybe B.ByteString
+hexToBase64 hexString = B64.encode <$> decodeHex hexString
 
-fixedXor :: ByteString -> ByteString -> Either String ByteString
-fixedXor bs1 bs2 =
-    B16.encode <$> do
-        decoded1 <- B16.decode bs1
-        decoded2 <- B16.decode bs2
-        return $ pack $ zipWith xor (unpack decoded1) (unpack decoded2)
+-- Apply XOR to two hex strings, the second of which is the key to be repeated
+applyXor :: Text -> Text -> Maybe B.ByteString
+applyXor hexString hexKey = do
+    decodedBs <- decodeHex hexString
+    decodedKey <- decodeHex hexKey
+    return $ B.pack $ zipWith xor (B.unpack decodedBs) (cycle $ B.unpack decodedKey)
+
+data CharFreq = CharFreq {character :: !Text, frequency :: !Double}
+    deriving (Generic, Show)
+
+instance CSV.FromRecord CharFreq
+
+-- Read character frequencies (English) from a file
+getCharFreqs :: FilePath -> IO (M.Map Word8 Double)
+getCharFreqs filepath = do
+    contents <- BL.readFile filepath
+    case CSV.decode NoHeader contents of
+        Left err -> error err
+        Right records -> return $ vectorToMap records
+  where
+    vectorToMap :: V.Vector CharFreq -> M.Map Word8 Double
+    vectorToMap = M.fromList . map (\(CharFreq c f) -> (B.head $ encodeUtf8 c, f)) . V.toList
+
+-- Score a string based on comprising characters' frequencies (English)
+charFreqScore :: M.Map Word8 Double -> B.ByteString -> Double
+charFreqScore charFreqs = B.foldl' runScore 0.0
+  where
+    runScore :: Double -> Word8 -> Double
+    runScore acc c = acc + M.findWithDefault 0.0 c charFreqs
